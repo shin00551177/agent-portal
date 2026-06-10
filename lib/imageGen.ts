72be3c06d75c2242779c6e95541250a7aa5e74f3
Shinnosuke Hori <shinnosuke.h@aiavatar.work>
2026-06-10 20:03:45 +0900

    perf: imagen-4.0-fast に変更（/bin/zsh.02/枚、SNS用途に十分）

--- lib/imageGen.ts
+++ lib/imageGen.ts
@@ -18,7 +18,7 @@ export async function generateImage(
 
   try {
     const res = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/imagen-4.0-generate-001:predict?key=${apiKey}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/imagen-4.0-fast-generate-001:predict?key=${apiKey}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
